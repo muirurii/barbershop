bfdb86866d1df3e249eeefe60cb45d7293314941
muirurii <pnkanuna@gmail.com>
2022-05-22 22:18:05 +0300

    changed scroll function

--- app.js
+++ app.js
@@ -1,52 +1,86 @@
-//Scroll functionality
-// window.addEventListener('scroll', () => {
-//     if (window.scrollY > 200) {
-//         document.querySelector('header').classList.add('scrolled');
-//         .classList.add('show-top');
-//     } else {
-//         document.querySelector('header').classList.remove('scrolled');
-//         document.querySelector('#to-top').classList.remove('show-top');
-//     }
-// });
-
-window.addEventListener('resize', () => {
-    console.log(document.querySelector('header h1').clientWidth)
-})
+//Header
 
 const heroText = document.querySelector('.highlight');
 const header = document.querySelector('header');
-const toTop = document.querySelector('#to-top');
 
 const observeHeader = new IntersectionObserver(entry => {
-    header.className = entry[0].isIntersecting ? null : 'scrolled';
+    return header.className = entry[0].isIntersecting ? null : 'scrolled';
 });
 
 observeHeader.observe(heroText);;
 
+//Scroll
 
-const headerHeight = header.offsetHeight;
+const scrollFunction = () => {
+    if (window.innerWidth > 720) {
+        window.addEventListener('scroll', () => {
+            document.querySelectorAll('main>section').forEach(sec => {
+                const scroll = window.scrollY;
+                const offsetTop = sec.offsetTop;
+                const height = sec.offsetHeight;
+                const headerHeight = header.offsetHeight;
 
-
-window.addEventListener('scroll', () => {
-    document.querySelectorAll('main>section').forEach(sec => {
-        const scroll = window.scrollY;
-        const offsetTop = sec.offsetTop;
-        const height = sec.offsetHeight;
-
-        if (scroll + headerHeight >= offsetTop && scroll <= height + (offsetTop)) {
-            const id = sec.getAttribute('id');
-            document.querySelectorAll('header nav ul li').forEach(li => {
-                li.classList.remove('active');
+                if (scroll + headerHeight >= offsetTop && scroll <= height + (offsetTop)) {
+                    const id = sec.getAttribute('id');
+                    document.querySelectorAll('header nav ul li').forEach(li => {
+                        li.classList.remove('active');
+                    });
+                    document.querySelector(`header nav ul li a[href*="${id}"]`).parentElement.classList.add('active');
+                    return;
+                }
             });
-            document.querySelector(`header nav ul li a[href*="${id}"]`).parentElement.classList.add('active');
-            return;
-        }
-    });
+        });
+    }
+}
+
+scrollFunction();
+window.addEventListener('resize', scrollFunction);
+
+//Hamburger
+
+const menu = document.querySelector('.menu-alt');
+const hamburger = document.querySelector('.hamburger');
+
+const closeMenu = () => {
+    hamburger.classList.remove('menu-closed');
+    menu.classList.remove('show');
+}
+
+hamburger.addEventListener('click', (e) => {
+    if (e.target.classList.contains('menu-closed')) {
+        closeMenu();
+    } else {
+        hamburger.classList.add('menu-closed');
+        menu.classList.add('show');
+    }
 });
+
+menu.querySelectorAll('a').forEach(li => {
+    li.addEventListener('click', closeMenu);
+});
+
 // Back to top
+
 document.querySelector('.fa-arrow-circle-up').addEventListener('click', () => {
     window.scrollTo(0, 0);
 });
+
+//about cards
+
+const aboutCards = document.querySelectorAll('.about-card');
+
+const aboutCardsObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('reveal');
+            aboutCardsObserver.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.4
+});
+
+aboutCards.forEach(card => aboutCardsObserver.observe(card));
 
 //testimonials
 
@@ -55,16 +89,15 @@ const testimonials = document.querySelectorAll('.testimonial');
 const testimonialObserver = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            entry.target.classList.remove('hidden');
+            entry.target.classList.add('show-test');
+            testimonialObserver.unobserve(entry.target);
         }
     });
 }, {
     threshold: 0
 });
 
-
 testimonials.forEach(testimonial => testimonialObserver.observe(testimonial))
-
 
 //feedback input effect
 
@@ -86,28 +119,6 @@ feedInput.addEventListener('blur', () => {
 document.querySelectorAll('form').forEach(form => form.addEventListener('submit', (e) => {
     e.preventDefault();
 }));
-
-//Hamburger
-const menu = document.querySelector('.menu-alt');
-const hamburger = document.querySelector('.hamburger');
-
-const closeMenu = () => {
-    hamburger.classList.remove('menu-closed');
-    menu.classList.remove('show');
-}
-
-hamburger.addEventListener('click', (e) => {
-    if (e.target.classList.contains('menu-closed')) {
-        closeMenu();
-    } else {
-        hamburger.classList.add('menu-closed');
-        menu.classList.add('show');
-    }
-});
-
-menu.querySelectorAll('a').forEach(li => {
-    li.addEventListener('click', closeMenu);
-});
 
 document.querySelector('.to-booking').addEventListener('click', () => {
     const bookingPagePosition = document.querySelector('#booking').offsetTop;
